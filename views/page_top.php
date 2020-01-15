54f409391309e5a8cfe1b329ca84566e6b75f409
Malav Nitin Patel <mpatel2@isimtl.local>
2020-01-15 14:22:13 -0500

    Add Some Files

---
+++ views/page_top.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?= $page_title ?></title> 
+    </head>
+    <body>
+        <header>
+            <h1><?= $page_title ?></h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="pagea.php">PAgea</a>
+                <a href="pageb.php">pageb</a>
+            </nav>
+            <div></div>
+        </header>
