c9b46e4b2a4666ff861924b08f532f78ea15e7e7
Mohit Kumar Mohit Kumar <mmohitku@isimtl.local>
2020-01-21 13:33:38 -0500

    Add form and validation in php and insert data in database

--- db/conn.php
+++ db/conn.php
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
+
         <?php
 
-            $mysqli = new mysqli("localhost", "root", "", "sitebuild");
+            $mysqli = new mysqli("localhost", "root", "", "finalproject");
             //var_dump($mysqli->connect_errno);
 
             if ($mysqli->connect_errno) {
                 echo "Failled to connect to MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
             }
        
-=======
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Connection</title> 
-    </head>
-    <body>
-        <header>
-            <h1> Code of connection to databases</h1>
-            
-            <div></div>
-        </header>
-        </body>
-</html>
->>>>>>> 54f409391309e5a8cfe1b329ca84566e6b75f409
+
+?>
--- tbl_product.php
+++ tbl_product.php
@@ -17,20 +17,16 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * from product";
+$sql = "SELECT * from products_table";
 $result = $conn->query($sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Id: " . $row["product_id"]. " - Name: " . $row["product_name"]. "-Price " . $row["price"]. "<br>";
+        echo "Id: " . $row["p_id"]. " - Name: " . $row["name"]. "-Price " . $row["price"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 $conn->close();
 ?>
-
-
-
-
