1c6f534575de01fe9993ddac121595d8baab1a4f
Karankumar Rakeshkumar Patel <kpatel2@isimtl.local>
2020-01-27 14:40:21 -0500

    final homepage,header and like function

---
+++ like.php
@@ -0,0 +1,41 @@
+<?php
+    $page_title = 'Products';
+    require_once('views/page_top.php');
+
+    include 'db/conn.php';
+
+    $id = $_GET['id'];
+
+    $sql = "SELECT n_likes from products_table WHERE p_id='".$id."'";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+
+    $old_likes = $row['n_likes'];
+
+    $old_likes+=1;
+
+    $sql1 = "UPDATE products_table SET n_likes='$old_likes' WHERE p_id='".$id."'";
+
+    if ($result = $conn->query($sql1)) { ?>
+        &nbsp;
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>Success!</strong> Data updated successfully.
+        </div><?php
+            } else {
+                ?>
+        &nbsp;
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <strong>Error!</strong> Data cannot be updated.
+        </div>
+        <?php
+            }
+            header("Location:index.php");
+            $conn->close();
+
+
+
+
+
+?>
---
+++ page_top.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <style>
+    .topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: black;
+  color:white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?= $page_title ?></title> 
+    </head>
+    <body>
+    <div class="topnav">
+  <a href="index.php">Home</a>
+  <a href="cart.php">Cart</a>
+  <!-- a style="float:right;" href="admin/login.php">Admin</a -->
+
+</div>
+</body>
+</html>
