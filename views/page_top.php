f40e3d19e35d01762c9160f0cf0aa36951905cd1
Karankumar Rakeshkumar Patel <kpatel2@isimtl.local>
2020-01-27 14:43:08 -0500

    final homepage,header and like function

--- views/page_top.php
+++ views/page_top.php
@@ -1,18 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+    <style>
+    .topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: black;
+  color:white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $page_title ?></title> 
     </head>
     <body>
-        <header>
-            <h1><?= $page_title ?></h1>
-            <nav>
-                <a href="index.php">Home</a>
-                <a href="pagea.php">PAgea</a>
-                <a href="pageb.php">pageb</a>
-                <a href="tbl_product.php">Products</a>
-            </nav>
-            <div></div>
-        </header>
+    <div class="topnav">
+  <a href="index.php">Home</a>
+  <a href="cart.php">Cart</a>
+  <!-- a style="float:right;" href="admin/login.php">Admin</a -->
+
+</div>
+</body>
+</html>
